7dc4ea9e428791d4b115dda967470703701b6577
Ben Greenman <benjaminlgreenman@gmail.com>
2017-11-05 00:17:15 -0400

    ** add basedir dependency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "with-cache")
 (define deps '("base" "typed-racket-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "pict-lib"))
+(define build-deps '("basedir" "scribble-lib" "racket-doc" "rackunit-lib" "pict-lib"))
 (define pkg-desc "Simple, filesystem-based caching")
 (define version "0.6")
 (define pkg-authors '(ben))
